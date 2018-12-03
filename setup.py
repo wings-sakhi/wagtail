e673160b0df04cd8b09836ae6582e043ff126615
Mukunth <mukunthkrishna@gmail.com>
2018-12-03 18:53:26 +0530

    fixed setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ import setuptools
 #     long_description = fh.read()
 
 setuptools.setup(
-     name='wagtail-wings',  
+     name='wagtail_wings',  
      version='0.1',
-     scripts=['wagtail'] ,
+     scripts=['wagtail_wings'] ,
      author="Mukunth Krishnasagar",
      author_email="mukunth@wings.lc",
      description="A Custom wagtail package with encryption",
-    #  long_description=long_description,
+    #long_description=long_description,
      long_description="Yet to be added",
      long_description_content_type="text/markdown",
      url="https://github.com/wings-sakhi/wagtail",
